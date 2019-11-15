238f569181ffacff08301178a627c73d3cb84c7d
kazuya kawaguchi <kawakazu80@gmail.com>
2019-11-15 18:28:48 +0900

    :up: updates

--- jest-preset.js
+++ jest-preset.js
@@ -1,6 +1,7 @@
 const preset = require('@vue/cli-plugin-unit-jest/jest-preset')
-const transformer = require('./lib/index').default
+const debug = require('debug')('vue-i18n-jest')
 
-preset.transform['^.+\\.vue$'] = transformer
+preset.transform['^.+\\.vue$'] = require.resolve('vue-i18n-jest')
+debug('vue-i18n-preset', preset)
 
 module.exports = preset
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import transform from './transform'
 
-export default transform
+export = transform

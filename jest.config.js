504eb3c5c4b706907b3db40db44bb89277ac191d
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-12-14 18:03:44 +0900

    test: ignore ./e2e on unit test

--- jest.config.js
+++ jest.config.js
@@ -154,6 +154,7 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     '/node_modules/',
+    './e2e/',
     './examples/'
   ]
 

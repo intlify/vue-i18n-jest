d21ed55e4cb10ac73618470ef67cbb6235f38414
XavierChevalier <mcmodz@hotmail.fr>
2021-11-30 17:19:06 +0100

    feat: support vue-jest@v5 (#23)
    Refs: #23

--- src/process.ts
+++ src/process.ts
@@ -14,36 +14,40 @@ type ProcessParameter = {
   /** All of the blocks matching your type, returned from `@vue/component-compiler-utils` */
   blocks: SFCCustomBlock[]
   /** The internal namespace for a component's Vue Options in vue-jest */
-  vueOptionsNamespace: string
+  componentNamespace: string
   /** The SFC file being processed */
   filename: string
 }
 
 const VUE_I18N_OPTION = '__i18n'
+const VUE_I18N_BLOCK_TYPE = 'i18n'
+
+type JsonI18nBlockMessages = Record<string, unknown>
 
 /**
  * Process vue-i18n contents inside of a custom block and prepare it for execution in a testing environment.
  */
-export default function process ({ blocks, vueOptionsNamespace, filename }: ProcessParameter) : string {
-  const i18nResources = blocks.map(block => {
-    if (block.type !== 'i18n') return
+export default function process ({ blocks, componentNamespace, filename }: ProcessParameter): string[] {
+  const i18nOption = `${componentNamespace}.${VUE_I18N_OPTION}`
+  const generatedCode = blocks
+    .reduce((blocksValues, block) => {
+      if (block.type !== VUE_I18N_BLOCK_TYPE) return blocksValues
 
-    const value = parseI18nBlockToJSON(block, filename)
-      .replace(/\u2028/g, '\\u2028') // LINE SEPARATOR
-      .replace(/\u2029/g, '\\u2029') // PARAGRAPH SEPARATOR
-      .replace(/\\/g, '\\\\')
-      .replace(/'/g, "\\'")
-    return `'${value}'`
-  }).filter((s): s is string => !!s)
+      const i18nBlockConfig = {
+        locale: (block.attrs && block.attrs.locale) || '',
+        resource: parseI18nBlockToJSON(block, filename)
+      }
 
-  // vueOptions.__i18n = [
-  // '<json encoded block 1>',
-  // '<json encoded block 2>'
-  // ]
-  const i18nOption = `${vueOptionsNamespace}.${VUE_I18N_OPTION}`
-  const code = i18nResources.length ? `${i18nOption} = [\n${i18nResources.join(',\n')}\n]` : ''
-  debug('generatedCode', code)
-  return code
+      return blocksValues.concat(`${i18nOption}.push(${JSON.stringify(i18nBlockConfig)});`)
+    }, [] as string[])
+
+  if (generatedCode.length > 0) {
+    generatedCode.unshift(`${i18nOption} = ${i18nOption} || [];`)
+  }
+
+  debug('generatedCode', generatedCode)
+
+  return generatedCode
 }
 
 /**
@@ -51,37 +55,31 @@ export default function process ({ blocks, vueOptionsNamespace, filename }: Proc
  * @param block SFC block returned from `@vue/component-compiler-utils`
  * @param filename The SFC file being processed
  */
-function parseI18nBlockToJSON (block: SFCCustomBlock, filename: string): string {
+function parseI18nBlockToJSON (block: SFCCustomBlock, filename: string): JsonI18nBlockMessages {
   const lang = block.attrs && block.attrs.lang
-  const locale = block.attrs && block.attrs.locale
   const src = block.attrs && block.attrs.src
   const content = src
     ? readFileSync(getAbsolutePath(src, filename)).toString()
     : block.content
 
-  return convertToJSON(content, lang, locale)
+  return convertToJSON(content, lang)
 }
 
 /**
  * Convert JSON/YAML/JSON5 to minified JSON string.
  * @param source JSON/YAML/JSON5 encoded string
  * @param lang Language used in `source`. Supported JSON, YAML or JSON5.
- * @param locale Attribute "locale" on <i18n> block will be added.
  * @returns {string} A minified JSON string
  */
-function convertToJSON (source: string, lang: string, locale: string): string {
-  const stringify = locale
-    ? (parseResult: any) => JSON.stringify({ [locale]: parseResult })
-    : JSON.stringify
-
+function convertToJSON (source: string, lang: string): JsonI18nBlockMessages {
   switch (lang) {
     case 'yaml':
     case 'yml':
-      return stringify(parseYAML(source))
+      return parseYAML(source) as JsonI18nBlockMessages
     case 'json5':
-      return stringify(parseJSON5(source))
+      return parseJSON5(source)
     default: // fallback to 'json'
-      return stringify(JSON.parse(source))
+      return JSON.parse(source)
   }
 }
 
